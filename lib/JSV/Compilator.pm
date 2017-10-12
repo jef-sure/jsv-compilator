ff89de8e5c971cf12a6b25717f3afb41c21faca0
Anton Petrusevich <anton@gitlab.lxc>
2017-10-12 19:24:10 +0200

    array validation, error reporting

--- lib/JSV/Compilator.pm
+++ lib/JSV/Compilator.pm
@@ -11,79 +11,79 @@ use Data::Dumper;
 use Regexp::Common('RE_ALL', 'Email::Address', 'URI', 'time');
 
 sub new {
-	my ($class, %args) = @_;
-	bless {
-		original_schema => {},
-		full_schema     => {}
-	}, $class;
+    my ($class, %args) = @_;
+    bless {
+        original_schema => {},
+        full_schema     => {}
+    }, $class;
 }
 
 sub load_schema {
-	my ($self, $file) = @_;
-	croak "Unreadable file" if !-r $file;
-	if ($file =~ /\.yaml$/i || $file =~ /\.yml$/i) {
-		require YAML::XS;
-		$self->{original_schema} = YAML::XS::LoadFile($file);
-	} elsif ($file =~ /\.json/i) {
-		$self->{original_schema} = decode_json(path($file)->slurp_raw);
-	} else {
-		croak "Unknown file type: must be .json or .yaml";
-	}
-	$self->{full_schema} = dclone $self->{original_schema};
-	walkdepth(
-		+{
-			wanted => sub {
-				if (   ref $_ eq "HASH"
-					&& exists $_->{'$ref'}
-					&& !ref $_->{'$ref'}
-					&& keys %$_ == 1)
-				{
-					my $rv = $_->{'$ref'};
-					$rv =~ s/.*#//;
-					my $rp = JSON::Pointer->get($self->{full_schema}, $rv);
-					if ('HASH' eq ref $rp) {
-						%$_ = %$rp;
-					}
+    my ($self, $file) = @_;
+    croak "Unreadable file" if !-r $file;
+    if ($file =~ /\.yaml$/i || $file =~ /\.yml$/i) {
+        require YAML::XS;
+        $self->{original_schema} = YAML::XS::LoadFile($file);
+    } elsif ($file =~ /\.json/i) {
+        $self->{original_schema} = decode_json(path($file)->slurp_raw);
+    } else {
+        croak "Unknown file type: must be .json or .yaml";
+    }
+    $self->{full_schema} = dclone $self->{original_schema};
+    walkdepth(
+        +{
+            wanted => sub {
+                if (   ref $_ eq "HASH"
+                    && exists $_->{'$ref'}
+                    && !ref $_->{'$ref'}
+                    && keys %$_ == 1)
+                {
+                    my $rv = $_->{'$ref'};
+                    $rv =~ s/.*#//;
+                    my $rp = JSON::Pointer->get($self->{full_schema}, $rv);
+                    if ('HASH' eq ref $rp) {
+                        %$_ = %$rp;
+                    }
 
-				}
-			},
-		},
-		$self->{full_schema}
-	);
-	return $self;
+                }
+            },
+        },
+        $self->{full_schema}
+    );
+    return $self;
 }
 
 sub compile {
-	my ($self, %opts) = @_;
-	local $self->{coersion} = $opts{coersion} // 0;
-	local $self->{to_json}  = $opts{to_json}  // 0;
-	local $self->{required_modules} = {};
-	my $input_sym = $opts{input_symbole} // '$_[0]';
-	my $schema    = $self->{full_schema};
-	my $type      = 'string';
-	$type = $schema->{type} // 'string';
-	my $is_required = $opts{is_required} // $type eq 'object' || 0;
-	my $val_func    = "_validate_$type";
-	my $val_expr    = $self->$val_func($input_sym, $schema, $is_required);
-	return "sub { $val_expr }";
+    my ($self, %opts) = @_;
+    local $self->{coersion} = $opts{coersion} // 0;
+    local $self->{to_json}  = $opts{to_json}  // 0;
+    local $self->{required_modules} = {};
+    my $input_sym = $opts{input_symbole} // '$_[0]';
+    my $schema    = $self->{full_schema};
+    my $type      = 'string';
+    $type = $schema->{type} // 'string';
+    my $is_required = $opts{is_required} // $type eq 'object' || 0;
+    my $val_func    = "_validate_$type";
+    my $val_expr    = $self->$val_func($input_sym, $schema, "", $is_required);
+    return $val_expr;
 }
 
 # type: six primitive types ("null", "boolean", "object", "array", "number", or "string"), or "integer"
 
 sub _sympt_to_path {
-	my ($sympt) = @_;
-	$sympt =~ s!^[^\{\}]+!/!;
-	$sympt =~ s![\{\}]+!/!g;
-	$sympt;
+    my ($sympt) = @_;
+    $sympt =~ s!^[^\{\}]+!/!;
+    $sympt =~ s![\{\}]+!/!g;
+    $sympt;
 }
 
 sub _quote_var {
-	my $s = $_[0];
-	my $d = Data::Dumper->new([$s]);
-	$d->Terse(1);
-	my $qs = $d->Dump;
-	substr($qs, -1, 1, '') if substr($qs, -1, 1) eq "\n";
-	return $qs;
+    my $s = $_[0];
+    my $d = Data::Dumper->new([$s]);
+    $d->Terse(1);
+    my $qs = $d->Dump;
+    substr($qs, -1, 1, '') if substr($qs, -1, 1) eq "\n";
+    return $qs;
 }
 
 #<<<
@@ -99,160 +99,221 @@ my %formats = (
 #>>>
 
 sub _validate_null {
-	my ($self, $sympt, $schmpt) = @_;
-	return " !defined($sympt) ";
+    my ($self, $sympt, $schmptm, $path) = @_;
+    return "push \@\$errors, '$path must be null' if defined($sympt);\n";
 }
 
 sub _validate_boolean {
-	my ($self, $sympt, $schmpt, $is_required) = @_;
-	my $r = '1 == 1';
-	if (defined $schmpt->{default}) {
-		my $val = _quote_var($schmpt->{default});
-		$r = " do { $sympt =  $val if not defined $sympt; 1 } ";
-	}
-	if ($is_required) {
-		$r = " defined($sympt) && ($r) ";
-	} else {
-		$r = " !defined($sympt) || defined($sympt) && ($r) ";
-	}
-	if (defined $schmpt->{const}) {
-		$r = " $r && $r == $schmpt->{const} ";
-	}
-	if ($self->{to_json}) {
-		$r = " $r && (($sympt)? \\1: \\0) ";
-	} else {
-		$r = " $r && $sympt ";
-	}
-	return "($r)";
+    my ($self, $sympt, $schmpt, $path, $is_required) = @_;
+    my $r = '';
+    if (defined $schmpt->{default}) {
+        my $val = _quote_var($schmpt->{default});
+        $r = "$sympt = $val if not defined $sympt;\n";
+    }
+    $r .= "if(defined($sympt)) {\n";
+    if (defined $schmpt->{const}) {
+        $r .= "  { no warnings 'undefined';\n";
+        $r .= "    push \@\$errors, '$path must be '.($schmpt->{const}?'true':'false') if $sympt != $schmpt->{const}\n";
+        $r .= "  }\n";
+    }
+    if ($self->{to_json}) {
+        $r = "  $sympt = (($sympt)? \\1: \\0);\n";
+    } elsif ($self->{coersion}) {
+        $r = "  $sympt = (($sympt)? 1: 0);\n";
+    }
+    $r .= "}\n";
+    if ($is_required) {
+        $r .= "else {\n";
+        $r .= "  push \@\$errors, '$path is required';\n";
+        $r .= "}\n";
+    }
+    return $r;
 }
 
 sub _validate_string {
-	my ($self, $sympt, $schmpt, $is_required) = @_;
-	my $r = '1 == 1';
-	if (defined $schmpt->{default}) {
-		my $val = _quote_var($schmpt->{default});
-		$r = " do { $sympt =  $val if not defined $sympt; 1 } ";
-	}
-	if (defined $schmpt->{maxLength}) {
-		$r = " $r && length($sympt) <= $schmpt->{maxLength} ";
-	}
-	if (defined $schmpt->{minLength}) {
-		$r = " $r && length($sympt) >= $schmpt->{minLength} ";
-	}
-	if (defined $schmpt->{const}) {
-		my $val = _quote_var($schmpt->{const});
-		$r = " $r && $r eq $val ";
-	}
-	if (defined $schmpt->{pattern}) {
-		my $pattern = $schmpt->{pattern};
-		$pattern =~ s/\\Q(.*?)(?:\\E|$)/quotemeta($1)/ge;
-		$r = " $r && $r =~ /$pattern/ ";
-	}
-	if ($schmpt->{enum} && 'ARRAY' eq ref($schmpt->{enum}) && @{$schmpt->{enum}}) {
-		my $can_list = join ", ", map {_quote_var($_)} @{$schmpt->{enum}};
-		$self->{required_modules}{'List::Util'}{any} = 1;
-		$r = " $r && (any {$_ eq $sympt} ($can_list)) ";
-	}
-	if ($schmpt->{format} && $formats{$schmpt->{format}}) {
-		$r = " $r && ($sympt =~ /^$formats{$schmpt->{format}}\$/) ";
-	}
-	if ($self->{to_json} || $self->{coersion}) {
-		$r = " $r && $sympt . \"\" eq $sympt ";
-	}
-	if ($is_required) {
-		$r = " (defined($sympt) && ($r)) ";
-	} else {
-		$r = " (!defined($sympt) || (defined($sympt) && ($r))) ";
-	}
-	return $r;
+    my ($self, $sympt, $schmpt, $path, $is_required) = @_;
+    my $r = '';
+    if (defined $schmpt->{default}) {
+        my $val = _quote_var($schmpt->{default});
+        $r = "$sympt = $val if not defined $sympt;\n";
+    }
+    $r .= "if(defined($sympt)) {\n";
+    if (defined $schmpt->{maxLength}) {
+        $r .= "  push \@\$errors, '$path length must be not greater than ";
+        $r .= "$schmpt->{maxLength}' if length($sympt) > $schmpt->{maxLength};\n";
+    }
+    if (defined $schmpt->{minLength}) {
+        $r .= "  push \@\$errors, '$path length must be not less than ";
+        $r .= "$schmpt->{minLength}' if length($sympt) < $schmpt->{minLength};\n";
+    }
+    if (defined $schmpt->{const}) {
+        my $val = _quote_var($schmpt->{const});
+        $r .= "  push \@\$errors, '$path must be $schmpt->{const}' if $sympt ne $val;\n";
+    }
+    if (defined $schmpt->{pattern}) {
+        my $pattern = $schmpt->{pattern};
+        $pattern =~ s/\\Q(.*?)(?:\\E|$)/quotemeta($1)/ge;
+        $r .= "  push \@\$errors, '$path does not match pattern' if $sympt !~ /$pattern/;\n";
+    }
+    if ($schmpt->{enum} && 'ARRAY' eq ref($schmpt->{enum}) && @{$schmpt->{enum}}) {
+        my $can_list = join ", ", map {_quote_var($_)} @{$schmpt->{enum}};
+        $self->{required_modules}{'List::Util'}{none} = 1;
+        $r .= "  push \@\$errors, '$path must be on of $can_list' if none {$_ eq $sympt} ($can_list);\n";
+    }
+    if ($schmpt->{format} && $formats{$schmpt->{format}}) {
+        $r .= "  push \@\$errors, '$path does not match format $schmpt->{format}'";
+        $r .= " if $sympt !~ /^$formats{$schmpt->{format}}\$/;\n";
+    }
+    if ($self->{to_json} || $self->{coersion}) {
+        $r .= "  $sympt = \"$sympt\";\n";
+    }
+    $r .= "}\n";
+    if ($is_required) {
+        $r .= "else {\n";
+        $r .= "  push \@\$errors, '$path is required';\n";
+        $r .= "}\n";
+    }
+    return $r;
 }
 
 sub _validate_any_number {
-	my ($self, $sympt, $schmpt, $is_required, $re) = @_;
-	my $r = '1 == 1';
-	if (defined $schmpt->{default}) {
-		my $val = _quote_var($schmpt->{default});
-		$r = " do { $sympt =  $val if not defined $sympt; 1 } ";
-	}
-	$r = " $r && ($sympt =~ /^$re\$/) ";
-	if (defined $schmpt->{minimum}) {
-		$r = " $r &&  $sympt >= $schmpt->{minimum} ";
-	}
-	if (defined $schmpt->{maximum}) {
-		$r = " $r &&  $sympt <= $schmpt->{maximum} ";
-	}
-	if (defined $schmpt->{exclusiveMinimum}) {
-		$r = " $r &&  $sympt > $schmpt->{exclusiveMinimum} ";
-	}
-	if (defined $schmpt->{exclusiveMaximum}) {
-		$r = " $r &&  $sympt < $schmpt->{exclusiveMaximum} ";
-	}
-	if (defined $schmpt->{const}) {
-		$r = " $r && $r == $schmpt->{const} ";
-	}
-	if ($schmpt->{multipleOf}) {
-		$self->{required_modules}{'POSIX'}{floor} = 1;
-		$r = " $r &&  $sympt / $schmpt->{multipleOf} ==  floor($sympt / $schmpt->{multipleOf}) ";
-	}
-	if ($schmpt->{enum} && 'ARRAY' eq ref($schmpt->{enum}) && @{$schmpt->{enum}}) {
-		my $can_list = join ", ", @{$schmpt->{enum}};
-		$self->{required_modules}{'List::Util'}{any} = 1;
-		$r = " $r && (any {$_ == $sympt} ($can_list)) ";
-	}
-	if ($schmpt->{format} && $formats{$schmpt->{format}}) {
-		$r = " $r && ($sympt =~ /^$formats{$schmpt->{format}}\$/) ";
-	}
-	if ($self->{to_json} || $self->{coersion}) {
-		$r = " $r && 0 + $sympt == $sympt ";
-	}
-	if ($is_required) {
-		$r = " (defined($sympt) && ($r)) ";
-	} else {
-		$r = " (!defined($sympt) || (defined($sympt) && ($r))) ";
-	}
-	return $r;
+    my ($self, $sympt, $schmpt, $path, $is_required, $re, $ntype) = @_;
+    my $r = '';
+    $ntype ||= '';
+    if (defined $schmpt->{default}) {
+        my $val = _quote_var($schmpt->{default});
+        $r = "$sympt = $val if not defined $sympt;\n";
+    }
+    $r .= "if(defined($sympt)) { {\n";
+    $r .= "  if($sympt !~ /^$re\$/){ push \@\$errors, '$path does not look like $ntype number'; last }\n";
+    if (defined $schmpt->{minimum}) {
+        $r .= "  push \@\$errors, '$path must be not less than $schmpt->{minimum}'";
+        $r .= " if $sympt < $schmpt->{minimum};\n";
+    }
+    if (defined $schmpt->{exclusiveMinimum}) {
+        $r .= "  push \@\$errors, '$path must be greater than $schmpt->{exclusiveMinimum}'";
+        $r .= " if $sympt <= $schmpt->{exclusiveMinimum};\n";
+    }
+    if (defined $schmpt->{maximum}) {
+        $r .= "  push \@\$errors, '$path must be not greater than $schmpt->{maximum}'";
+        $r .= " if $sympt > $schmpt->{maximum};\n";
+    }
+    if (defined $schmpt->{exclusiveMaximum}) {
+        $r .= "  push \@\$errors, '$path must be less than $schmpt->{exclusiveMaximum}'";
+        $r .= " if $sympt >= $schmpt->{exclusiveMaximum};\n";
+    }
+    if (defined $schmpt->{const}) {
+        $r .= "  push \@\$errors, '$path must be $schmpt->{const}' if $sympt != $schmpt->{const};\n";
+    }
+    if ($schmpt->{multipleOf}) {
+        $self->{required_modules}{'POSIX'}{floor} = 1;
+        $r .= "  push \@\$errors, '$path must be multiple of $schmpt->{multipleOf}'";
+        $r .= " if $sympt / $schmpt->{multipleOf} !=  floor($sympt / $schmpt->{multipleOf});\n";
+    }
+    if ($schmpt->{enum} && 'ARRAY' eq ref($schmpt->{enum}) && @{$schmpt->{enum}}) {
+        my $can_list = join ", ", map {_quote_var($_)} @{$schmpt->{enum}};
+        $self->{required_modules}{'List::Util'}{none} = 1;
+        $r .= "  push \@\$errors, '$path must be on of $can_list' if none {$_ == $sympt} ($can_list);\n";
+    }
+    if ($schmpt->{format} && $formats{$schmpt->{format}}) {
+        $r .= "  push \@\$errors, '$path does not match format $schmpt->{format}'";
+        $r .= " if $sympt !~ /^$formats{$schmpt->{format}}\$/;\n";
+    }
+    if ($self->{to_json} || $self->{coersion}) {
+        $r .= "  $sympt += 0;\n";
+    }
+    $r .= "} }\n";
+    if ($is_required) {
+        $r .= "else {\n";
+        $r .= "  push \@\$errors, '$path is required';\n";
+        $r .= "}\n";
+    }
+    return $r;
 
 }
 
 sub _validate_number {
-	my ($self, $sympt, $schmpt, $is_required) = @_;
-	return $self->_validate_any_number($sympt, $schmpt, $is_required, $RE{num}{real});
+    my ($self, $sympt, $schmpt, $path, $is_required) = @_;
+    return $self->_validate_any_number($sympt, $schmpt, $path, $is_required, $RE{num}{real});
 }
 
 sub _validate_integer {
-	my ($self, $sympt, $schmpt, $is_required) = @_;
-	return $self->_validate_any_number($sympt, $schmpt, $is_required, $RE{num}{int});
+    my ($self, $sympt, $schmpt, $path, $is_required) = @_;
+    return $self->_validate_any_number($sympt, $schmpt, $path, $is_required, $RE{num}{int}, "integer");
 }
 
 sub _validate_object {
-	my ($self, $sympt, $schmpt, $is_required) = @_;
-	my $r = '1 == 1';
-	if ($schmpt->{default}) {
-		my $val = _quote_var($schmpt->{default});
-		$r = " do { $sympt =  $val if not defined $sympt; 1 } ";
-	}
-	$r = " $r && 'HASH' eq ref($sympt) ";
-	if ($schmpt->{properties} && 'HASH' eq ref $schmpt->{properties}) {
-		my %required;
-		if ($schmpt->{required} && 'ARRAY' eq ref $schmpt->{required}) {
-			%required = map {$_ => undef} @{$schmpt->{required}};
-		}
-		my @r;
-		for my $k (keys %{$schmpt->{properties}}) {
-			my $type = 'string';
-			if ('HASH' eq ref $schmpt->{properties}{$k}) {
-				$type = $schmpt->{properties}{$k}{type} // 'string';
-			}
-			my $val_func = "_validate_$type";
-			push @r, $self->$val_func("${sympt}->{$k}", $schmpt->{properties}{$k}, $required{$k}) . "\n";
-		}
-		$r = join " && ", $r, @r;
-	}
-	if ($is_required) {
-		$r = " (defined($sympt) && ($r)) ";
-	} else {
-		$r = " (!defined($sympt) || (defined($sympt) && ($r))) ";
-	}
+    my ($self, $sympt, $schmpt, $path, $is_required) = @_;
+    my $r = '';
+    if ($schmpt->{default}) {
+        my $val = _quote_var($schmpt->{default});
+        $r = "  $sympt = $val if not defined $sympt;\n";
+    }
+    $r .= "if('HASH' eq ref($sympt)) {\n";
+    if ($schmpt->{properties} && 'HASH' eq ref $schmpt->{properties}) {
+        my %required;
+        if ($schmpt->{required} && 'ARRAY' eq ref $schmpt->{required}) {
+            %required = map {$_ => 1} @{$schmpt->{required}};
+        }
+        for my $k (keys %{$schmpt->{properties}}) {
+            my $type = 'string';
+            if ('HASH' eq ref $schmpt->{properties}{$k}) {
+                $type = $schmpt->{properties}{$k}{type} // 'string';
+            }
+            my $val_func = "_validate_$type";
+            $r .= $self->$val_func("${sympt}->{$k}", $schmpt->{properties}{$k}, "$path/$k", $required{$k});
+        }
+    }
+    $r .= "}\n";
+    if ($is_required) {
+        $path = "object" if $path eq "";
+        $r .= "else {\n";
+        $r .= "  push \@\$errors, '$path is required';\n";
+        $r .= "}\n";
+    }
+    return $r;
+}
+
+sub _validate_array {
+    my ($self, $sympt, $schmpt, $path, $is_required) = @_;
+    my $r = '';
+    if ($schmpt->{default}) {
+        my $val = _quote_var($schmpt->{default});
+        $r = "  $sympt = $val if not defined $sympt;\n";
+    }
+    $r .= "if('ARRAY' eq ref($sympt)) {\n";
+    if (defined $schmpt->{minItems}) {
+        $r .= "  push \@\$errors, '$path must contain not less than $schmpt->{minItems} items'";
+        $r .= " if \@{$sympt} < $schmpt->{minItems};\n";
+    }
+    if (defined $schmpt->{maxItems}) {
+        $r .= "  push \@\$errors, '$path must contain not more than $schmpt->{maxItems} items'";
+        $r .= " if \@{$sympt} > $schmpt->{maxItems};\n";
+    }
+    if (defined $schmpt->{uniqueItems}) {
+        $r .= "  { my %seen;\n";
+        $r .= "    for (\@{$sympt}) {\n";
+        $r .= "      if(\$seen{\$_}) { push \@\$errors, '$path must contain only unique items'; last }\n";
+        $r .= "      \$seen{\$_} = 1;\n";
+        $r .= "    };\n";
+        $r .= "  }\n";
+    }
+    if ($schmpt->{items}) {
+        my $type     = $schmpt->{items}{type} // 'string';
+        my $val_func = "_validate_$type";
+        my $ivf      = $self->$val_func("\$_[0]", $schmpt->{items}, "$path/[]", $is_required);
+        $r .= "  { my \$tf = sub { $ivf };\n";
+        $r .= "    \$tf->(\$_) for (\@{$sympt});\n";
+        $r .= "  }\n";
+    }
+    $r .= "}\n";
+    if ($is_required) {
+        $path = "array" if $path eq "";
+        $r .= "else {\n";
+        $r .= "  push \@\$errors, '$path is required';\n";
+        $r .= "}\n";
+    }
+    return $r;
 }
 
 1;
--- t/01_basic.t
+++ t/01_basic.t
@@ -6,86 +6,71 @@ use JSV::Compilator;
 
 my $jsc = JSV::Compilator->new();
 $jsc->{full_schema} = {
-	"\$schema"    => "http://json-schema.org/draft-06/schema#",
-	"title"       => "Product",
-	"description" => "A product from Acme's catalog",
-	"type"        => "object",
-	"properties"  => {
-		"id" => {
-			"description" => "The unique identifier for a product",
-			"type"        => "integer"
-		},
-		"name" => {
-			"description" => "Name of the product",
-			"type"        => "string"
-		},
-		"price" => {
-			"type"             => "number",
-			"exclusiveMinimum" => 0
-		},
-		#		"tags" => {
-		#			"type"        => "array",
-		#			"items"       => {"type" => "string"},
-		#			"minItems"    => 1,
-		#			"uniqueItems" => 1
-		#			}
-	},
-	"required" => ["id", "name", "price"]
+    "\$schema"    => "http://json-schema.org/draft-06/schema#",
+    "title"       => "Product",
+    "description" => "A product from Acme's catalog",
+    "type"        => "object",
+    "properties"  => {
+        "id" => {
+            "description" => "The unique identifier for a product",
+            "type"        => "integer"
+        },
+        "name" => {
+            "description" => "Name of the product",
+            "type"        => "string"
+        },
+        "price" => {
+            "type"             => "number",
+            "exclusiveMinimum" => 0
+        },
+        "tags" => {
+            "type"        => "array",
+            "items"       => {"type" => "string"},
+            "minItems"    => 1,
+            "uniqueItems" => 1
+        }
+    },
+    "required" => ["id", "name", "price"]
 };
 
 my $ok_products = [
-	{   "id"    => 2,
-		"name"  => "An ice sculpture",
-		"price" => 12.50,
-	},
-	{   "id"    => 3,
-		"name"  => "A blue mouse",
-		"price" => 25.50,
-	}
+    {   "id"    => 2,
+        "name"  => "An ice sculpture",
+        "price" => 12.50,
+    },
+    {   "id"    => 3,
+        "name"  => "A blue mouse",
+        "price" => 25.50,
+        tags    => ["a1", "a2"],
+    }
 ];
 
 my $bad_products = [
-	{   "id"    => 2.5,
-		"name"  => "An ice sculpture",
-		"price" => 1,
-	},
-	{   "id"    => 3,
-		"name"  => "A blue mouse",
-		"price" => -1,
-	}
+    {   "id"    => 2.5,
+        "name"  => "An ice sculpture",
+        "price" => 1,
+        tags    => [],
+    },
+    {   "id"    => 3,
+        "name"  => "A blue mouse",
+        "price" => -1,
+        tags    => [1, 2, 3, 4, 1],
+    }
 ];
 
 my $res = $jsc->compile();
 ok($res, "Compiled");
-
-my $test_sub = eval $res;
+my $test_sub = eval "sub { my \$errors = []; $res; print \"\@\$errors\\n\" if \@\$errors; return \@\$errors == 0 }";
 is($@, '', "Successfully compiled");
 explain $res if $@;
 
 for my $p (@$ok_products) {
-	ok($test_sub->($p), "Tested product");
+    ok($test_sub->($p), "Tested product");
 }
 
 for my $p (@$bad_products) {
-	ok(!$test_sub->($p), "Tested product") or explain $res;
+    ok(!$test_sub->($p), "Tested product") or explain $res;
 }
-explain $res;
-my $s = sub {
-	!defined($_[0])
-		|| defined($_[0])
-		&& (
-		1 == 1 && 'HASH' eq ref($_[0]) && !defined($_[0]->{price})
-		|| defined($_[0]->{price})
-		&& (1 == 1
-			&& ($_[0]->{price}
-				=~ /^(?:(?i)(?:[-+]?)(?:(?=[.]?[0123456789])(?:[0123456789]*)(?:(?:[.])(?:[0123456789]{0,}))?)(?:(?:[E])(?:(?:[-+]?)(?:[0123456789]+))|))$/
-			)
-		)
-		&& !defined($_[0]->{id})
-		|| defined($_[0]->{id}) && (1 == 1 && ($_[0]->{id} =~ /^(?:(?:[-+]?)(?:[0123456789]+))$/))
-		 && !defined($_[0]->{name})
-		|| defined($_[0]->{name}) && (1 == 1)
-		);
-};
+#explain $res;
 
 done_testing();
